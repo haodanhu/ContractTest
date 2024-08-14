b8714b2cc00a0bbcbb94ad9e691090ae86d90fb0
haodanhu <haodanhu@gmail.com>
2024-08-14 14:42:59 +0800

    new file:   README.md 	new file:   my-twa/.gitignore 	new file:   my-twa/README.md 	new file:   my-twa/eslint.config.js 	new file:   my-twa/index.html 	new file:   my-twa/package-lock.json 	new file:   my-twa/package.json 	new file:   my-twa/public/tonconnect-manifest.json 	new file:   my-twa/public/vite.svg 	new file:   my-twa/src/App.css 	new file:   my-twa/src/App.tsx 	new file:   my-twa/src/assets/react.svg 	new file:   my-twa/src/contracts/Counter.ts 	new file:   my-twa/src/hooks/useAsyncInitialize.ts 	new file:   my-twa/src/hooks/useCounterContract.ts 	new file:   my-twa/src/hooks/useTonClient.ts 	new file:   my-twa/src/hooks/useTonConnect.ts 	new file:   my-twa/src/index.css 	new file:   my-twa/src/main.tsx 	new file:   my-twa/src/vite-env.d.ts 	new file:   my-twa/tsconfig.app.json 	new file:   my-twa/tsconfig.json 	new file:   my-twa/tsconfig.node.json 	new file:   my-twa/vite.config.ts

---
+++ my-twa/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react'
+import { useCounterContract } from './hooks/useCounterContract';
+import { useTonConnect } from './hooks/useTonConnect';
+
+
+function App() {
+  const { connected } = useTonConnect();
+  const { value, address, sendIncrement } = useCounterContract();
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div className='App'>
+      <div className='Container'>
+        <TonConnectButton />
+
+        <div className='Card'>
+          <b>Counter Address</b>
+          <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{value ?? 'Loading...'}</div>
+        </div>
+
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+        <a
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+          onClick={() => {
+            sendIncrement();
+          }}
+        >
+          Increment
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App
